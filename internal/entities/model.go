0779103dc927fe091dcce4cfb6f2d819cc2b9051
brenobmoreira <brenobmoreira20@gmail.com>
2025-10-06 21:30:07 -0300

    creating structs

---
+++ internal/entities/model.go
@@ -0,0 +1,27 @@
+package entities
+
+type Estabelecimento struct {
+	ID              string `dbase:"CNES"`
+	CodigoMunicipio string `dbase:"CODUFMUN"`
+}
+
+type Equipamentos struct {
+	ID                  string `dbase:"CNES"`
+	CodigoEquipamento   string `dbase:"TIPEQUIP"`
+	QuantidadeExistente string `dbase:"QT_EXIST"`
+	QuantidadeUso       string `dbase:"QT_USO"`
+}
+
+type EstabelecimentoCadastro struct {
+	ID         string `dbase:"CNES"`
+	Nome       string `dbase:"FANTASIA"`
+	Logradouro string `dbase:"NO_LOGRAD"`
+	Numero     string `dbase:"NUMERO_END"`
+	Bairro     string `dbase:"NO_BAIRRO"`
+	CEP        string `dbase:"CO_CEP"`
+}
+
+type EquipamentoDescricao struct {
+	Codigo    string `dbase:"TIPEQUIP"`
+	Descricao string `dbase:"DS_TPEQUIP"`
+}
