38b58875acb910b1b5eab712add49b21dbd8c4b5
brenobmoreira <brenobmoreira20@gmail.com>
2025-12-16 16:24:22 -0300

    improving sync

--- internal/handlers/sync.go
+++ internal/handlers/sync.go
@@ -12,73 +12,136 @@ import (
 	"github.com/brenobmoreira/go-datasus-etl/internal/parser"
 	"github.com/brenobmoreira/go-datasus-etl/internal/repository"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 )
+
+func ReturnRootDir() (rootDir string) {
+	rootDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	return rootDir
+}
 
 func SyncDatabase(repo *repository.Repo) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		rootDir, err := os.Getwd()
+		var g errgroup.Group
+
+		err := SyncCadastro(repo)
 		if err != nil {
-			panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
-		competencia := time.Now()
-		var wg sync.WaitGroup
-		blast_path := filepath.Join(rootDir, "internal", "parser", "blast-dbf")
-
-		cadastroChan := make(chan entities.EstabelecimentoCadastro)
-		archive_cd := "CADGERSC"
-		wg.Go(func() {
-			if err := repo.SalvarCadastro(cadastroChan); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
+		g.Go(func() error {
+			return SyncEstabelecimento(repo)
 		})
-		parser.CadastroParser(archive_cd, blast_path, rootDir, cadastroChan)
-		close(cadastroChan)
-		wg.Wait()
-		fmt.Println("Cadastro processado com sucesso!")
-		c.JSON(http.StatusOK, "Cadastro processado com sucesso!")
 
-		estabChan := make(chan entities.Estabelecimento)
-		archive_st := "ST/STSC2501"
-		wg.Go(func() {
-			if err := repo.SalvarEstabelecimento(estabChan); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
+		g.Go(func() error {
+			return SyncEquipamento(repo)
 		})
-		parser.EstabelecimentoParser(archive_st, blast_path, rootDir, competencia, estabChan)
-		close(estabChan)
-		wg.Wait()
-		fmt.Println("Estabelecimentos processados com sucesso!")
-		c.JSON(http.StatusOK, "Estabelecimentos processados com sucesso!")
 
-		equipChan := make(chan entities.Equipamentos)
-		archive_eq := "EQ/EQSC2501"
-		wg.Go(func() {
-			if err := repo.SalvarEquipamento(equipChan); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
+		g.Go(func() error {
+			return SyncDescricao(repo)
 		})
-		parser.EquipamentoParser(archive_eq, blast_path, rootDir, competencia, equipChan)
-		close(equipChan)
-		wg.Wait()
-		fmt.Println("Equipamentos processados com sucesso!")
-		c.JSON(http.StatusOK, "Equipamentos processados com sucesso!")
 
-		descricaoChan := make(chan entities.EquipamentoDescricao)
-		archive_desc := "TP_EQUIPAM"
-		wg.Go(func() {
-			if err := repo.SalvarDescricao(descricaoChan); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-		})
-		parser.DescricaoParser(archive_desc, blast_path, rootDir, descricaoChan)
-		close(descricaoChan)
-		wg.Wait()
-		fmt.Println("Descrições processadas com sucesso!")
-		c.JSON(http.StatusOK, "Descrições processadas com sucesso!")
+		if err := g.Wait(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Sync completo com sucesso!"})
+
 	}
+}
+
+func SyncCadastro(repo *repository.Repo) error {
+	rootDir := ReturnRootDir()
+	var wg sync.WaitGroup
+	blast_path := filepath.Join(rootDir, "internal", "parser", "blast-dbf")
+
+	cadastroChan := make(chan entities.EstabelecimentoCadastro)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := repo.SalvarCadastro(cadastroChan); err != nil {
+			fmt.Printf("Erro ao salvar cadastro: %v\n", err)
+		}
+	}()
+
+	parser.CadastroParser("CADGERSC", blast_path, rootDir, cadastroChan)
+	close(cadastroChan)
+	wg.Wait()
+
+	return nil
+}
+
+func SyncEstabelecimento(repo *repository.Repo) error {
+	rootDir := ReturnRootDir()
+	var wg sync.WaitGroup
+	blast_path := filepath.Join(rootDir, "internal", "parser", "blast-dbf")
+	competencia := time.Now()
+
+	estabChan := make(chan entities.Estabelecimento)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := repo.SalvarEstabelecimento(estabChan); err != nil {
+			fmt.Printf("Erro ao salvar estabelecimento: %v\n", err)
+		}
+	}()
+
+	parser.EstabelecimentoParser("ST/STSC2501", blast_path, rootDir, competencia, estabChan) // TODO: change path
+	close(estabChan)
+	wg.Wait()
+
+	return nil
+}
+
+func SyncEquipamento(repo *repository.Repo) error {
+	rootDir := ReturnRootDir()
+	var wg sync.WaitGroup
+	blast_path := filepath.Join(rootDir, "internal", "parser", "blast-dbf")
+	competencia := time.Now()
+
+	equipChan := make(chan entities.Equipamentos)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := repo.SalvarEquipamento(equipChan); err != nil {
+			fmt.Printf("Erro ao salvar equipamento: %v\n", err)
+		}
+	}()
+
+	parser.EquipamentoParser("EQ/EQSC2501", blast_path, rootDir, competencia, equipChan) // TODO: change path
+	close(equipChan)
+	wg.Wait()
+
+	return nil
+}
+
+func SyncDescricao(repo *repository.Repo) error {
+	rootDir := ReturnRootDir()
+	var wg sync.WaitGroup
+	blast_path := filepath.Join(rootDir, "internal", "parser", "blast-dbf")
+
+	descChan := make(chan entities.EquipamentoDescricao)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := repo.SalvarDescricao(descChan); err != nil {
+			fmt.Printf("Erro ao salvar descrição: %v\n", err)
+		}
+	}()
+
+	parser.DescricaoParser("TP_EQUIPAM", blast_path, rootDir, descChan)
+	close(descChan)
+	wg.Wait()
+
+	return nil
 }
